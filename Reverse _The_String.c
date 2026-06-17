80333401c655c05dfb56bc670a3694422abc7f6b
Sundaravaradhan A <sundaravaradhanmadurai@gmail.com>
2026-06-17 11:34:41 +0530

    Modify string reversal to change word order
    Updated the string reversal logic to maintain word order.

---
+++ Reverse _The_String.c
@@ -0,0 +1,34 @@
+// Reverse the String but words should change.
+
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+	char name[100]= "have a nice day";
+	//char name[100]= "Hello from Sundaravaradhan A";
+	int strlen;
+	//printf("%s",name);
+	
+	// string lenght
+	for (int i=0; name[i] != '\0'; i++){
+		strlen++;
+	}
+	int end =strlen;
+	//printf("%d", strlen);
+	//printf("%c",name[strlen-1]); // show i
+	for(int i=strlen;i>=0;i--){
+		//printf("%c",name[i]);
+		if(isspace((unsigned char)name[i])){
+			name[i]='\0';
+			
+			printf("%s ",&name[i+1]);
+		}
+		
+	}
+	printf("%s\n",name);
+
+	return 0;
+	
+}                                                                      
+// OUTPUT
+// day nice a have
